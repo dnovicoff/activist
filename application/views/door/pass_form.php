5fe65c37cd5ad9722c132e8a7f9f5cf0b07af277
David Novicoff <dnovicoff@comedian.(none)>
2019-06-17 14:26:31 -0500

    Codeigniter usage

--- application/views/door/pass_form.php
+++ application/views/door/pass_form.php
@@ -2,14 +2,35 @@
 <div classs="container">
 	<?php echo form_open('forms/pass'); ?>
 		<table width="100%">
-			<tr>
-				<td><label form="email">Email</label></td><td><input type="text" name="email" /></td>
-			<td>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_label("Email", "email", $attributes);
+			?>
+			</td><td>
+			<?php
+				$data = array(
+					'name' => 'email',
+					'id' => '',
+					'value' => '',
+					'class' => '',
+					'placeholder' => ''
+				);
+				echo form_input($data);
+			?>
+			</td><td>
 				<?php if (!empty(form_error('email')))  {  echo set_value('email').' '.form_error('email');  } ?>
-			</td>
-			</tr><tr>
-				<td><input type="submit" value="submit" name="submit" /></td>
-			</tr>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_submit("submit", "Submit", $attributes);
+			?>
+			</td></tr>
 		</table>
-	</form>
+	<?php  echo form_close(); ?>
 </div>
