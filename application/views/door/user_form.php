55c75fa7b7047729b00d5d75bf56c233b70060d5
David Novicoff <dnovicoff@comedian.(none)>
2019-06-17 14:26:53 -0500

    Codeigniter usage

--- application/views/door/user_form.php
+++ application/views/door/user_form.php
@@ -3,30 +3,107 @@
 <div class="container">
 	<?php echo form_open('forms/user'); ?>
 		<table width="100%">
-			<tr>
-			<td><label form="email">Email</label></td><td> <input type="text" name="email" value="<?php echo set_value('email'); ?>" /></td>
-			<td>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_label("Email", "email", $attributes);
+			?>
+			</td><td>
+			<?php
+				$data = array(
+					'name' => 'email',
+					'id' => '',
+					'value' => set_value('email'),
+					'class' => '',
+					'placeholder' => ''
+				);
+				echo form_input($data);
+			?>
+			</td><td>
 				<?php if (!empty(form_error('email')))  {  echo form_error('email');  } ?>
-			</td>
-			</tr><tr>
-			<td><label form="email">Confirm Email</label></td><td>
-				<input type="text" name="confirmemail" value="<?php echo set_value('confirmemail'); ?>" /></td>
-			<td>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_label("Confirm Email", "email", $attributes);
+			?>
+			</td><td>
+			<?php
+				$data = array(
+					'name' => 'confirmemail',
+					'id' => '',
+					'value' => set_value('confirmemail'),
+					'class' => '',
+					'placeholder' => ''
+				);
+				echo form_input($data);
+			?>
+			</td><td>
 				<?php if (!empty(form_error('confirmemail')))  {  echo form_error('confirmemail');  } ?>
-			</td>
-			</tr><tr>
-			<td><label form="password">Password</label></td><td> <input type="password" name="password" /></td>
-			<td>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_label("Password", "password", $attributes);
+			?>
+			</td><td>
+			<?php
+				$data = array(
+					'type' => 'password',
+					'name' => 'password',
+					'id' => '',
+					'value' => '',
+					'class' => '',
+					'placeholder' => ''
+				);
+				echo form_input($data);
+			?>
+			</td><td>
 				<?php if (!empty(form_error('password')))  {  echo form_error('password');  } ?>
-			</td>
-			</tr><tr>
-			<td><label form="password">Confirm Password</label></td><td> <input type="password" name="confirmpassword" /></td>
-			<td>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_label("Confirm Password", "password", $attributes);
+			?>
+			</td><td>
+			<?php
+				$data = array(
+					'type' => 'password',
+					'name' => 'confirmpassword',
+					'id' => '',
+					'value' => '',
+					'class' => '',
+					'placeholder' => ''
+				);
+				echo form_input($data);
+			?>
+			</td><td>
 				<?php if (!empty(form_error('confirmpassword')))  {  echo form_error('confirmpassword');  } ?>
-			</td>
-			</tr><tr>
-			<td><input type="reset" name="reset" /></td><td><input type="submit" value="submit" name="submit" /></td>
-			</tr>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_reset("reset", "Reset", $attributes);
+			?>
+			</td><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_submit("submit", "Submit", $attributes);
+			?>
+			</td></tr>
 		</table>
-	</form>
+	<?php  echo form_close(); ?>
 </div>
