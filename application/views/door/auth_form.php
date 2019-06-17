dc02e88867e2cbb2d2ebc1d97382cb12309b6696
David Novicoff <dnovicoff@comedian.(none)>
2019-06-17 14:26:08 -0500

    Codeigniter usage

--- application/views/door/auth_form.php
+++ application/views/door/auth_form.php
@@ -1,15 +1,63 @@
 <div class="container">
 	<?php echo form_open('forms/auth'); ?>
 		<table width="100%">
-			<tr>Email</tr>
-			<tr><input type="text" name="email"></tr>
-			<tr><?php if (!empty(form_error('email')))  {  echo form_error('email');  } ?></tr>
-			<tr>Password</tr>
-			<tr><input type="password" name="pass"></tr>
-			<tr><?php if (!empty(form_error('pass')))  {  echo form_error('pass');  } ?></tr>
-			<tr><input type="submit" value="Submit" name="submit" /></tr>
+			<tr>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_label("Email", "email", $attributes);
+			?>
+			</tr>
+			<tr>
+			<?php
+				$data = array(
+					'name' => 'email',
+					'id' => '',
+					'value' => '',
+					'class' => '',
+					'placeholder' => ''
+				);
+				echo form_input($data);
+			?>
+			</tr>
+			<tr>
+				<?php if (!empty(form_error('email')))  {  echo form_error('email');  } ?>
+			</tr>
+			<tr>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_label("Password", "password", $attributes);
+			?>
+			</tr>
+			<tr>
+			<?php
+				$data = array(
+					'type' => 'password',
+					'name' => 'pass',
+					'id' => '',
+					'value' => '',
+					'class' => '',
+					'placeholder' => ''
+				);
+				echo form_input($data);
+			?>
+			</tr>
+			<tr>
+				<?php if (!empty(form_error('pass')))  {  echo form_error('pass');  } ?>
+			</tr>
+			<tr>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_submit("submit", "Submit", $attributes);
+			?>
+			</tr>
 		</table>
-	</form>
+	<?php  echo form_close(); ?>
 	<a href="/pass/"">Password</a><br />
 	<a href="/user/">New User</a>
 </div>
