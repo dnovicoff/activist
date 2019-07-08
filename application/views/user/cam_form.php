4a2a56cb9156379b3d98f7f2c73414c056b4349f
David Novicoff <dnovicoff@comedian.(none)>
2019-07-08 16:06:49 -0500

    Creating user campaign form

--- application/views/user/cam_form.php
+++ application/views/user/cam_form.php
@@ -8,6 +8,60 @@
 				$attributes = array(
 					'class' => ''
 				);
+				echo "Start Date: ";
+			?>
+			</td><td>
+			<?php
+				$data = array(
+					'name' => 'start_date',
+					'id' => '',
+					'value' => set_value('start_date'),
+					'class' => '',
+					'placeholder' => 'yyyy-mm-dd'
+				);
+				echo form_input($data);
+			?>
+			</td><td>
+			<?php
+				if (!empty(form_error('start_date')))  {
+					echo '<div style="font-size: 80%; color: red;">'.
+						form_error('start_date').
+					'</div>';  
+				}
+			?>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo "End Date: ";
+			?>
+			</td><td>
+			<?php
+				$data = array(
+					'name' => 'end_date',
+					'id' => '',
+					'value' => set_value('end_date'),
+					'class' => '',
+					'placeholder' => 'yyyy-mm-dd'
+				);
+				echo form_input($data);
+			?>
+			</td><td>
+			<?php
+				if (!empty(form_error('end_date')))  {
+					echo '<div style="font-size: 80%; color: red;">'.
+						form_error('end_date').
+					'</div>';  
+				}
+			?>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
 				echo "Title: ";
 			?>
 			</td><td>
@@ -28,6 +82,48 @@
 						form_error('title').
 					'</div>';  
 				}
+			?>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo "Text ";
+			?>
+			</td><td>
+			<?php
+				$data = array(
+					'name' => 'cam_test',
+					'id' => '',
+					'value' => set_value('cam_text'),
+					'class' => '',
+					'placeholder' => 'Campaign description ...'
+				);
+				echo form_textarea($data);
+			?>
+			</td><td>
+			<?php
+				if (!empty(form_error('cam_text')))  {
+					echo '<div style="font-size: 80%; color: red;">'.
+						form_error('cam_text').
+					'</div>';  
+				}
+			?>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_reset("reset", "Reset", $attributes);
+			?>
+			</td><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_submit("submit", "Submit", $attributes);
 			?>
 			</td></tr>
 		</table>
