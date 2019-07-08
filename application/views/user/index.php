e0689bbe87f5e0657fba7328f20f1e3f35c4b294
David Novicoff <dnovicoff@comedian.(none)>
2019-07-07 22:28:50 -0500

    User forms

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,8 +1,8 @@
 
 <div class="container">
 	<div class="smallcol">
-		<a href="/user/loc_form">Create Location</a><br />
-		<a href="/user/cam_form">Create Campaign</a><br />
+		<a href="/user/loc">Create Location</a><br />
+		<a href="/user/cam">Create Campaign</a><br />
 		<a href="/user/logout">Logout</a><br />
 	</div>
 	<div class="largecol">
