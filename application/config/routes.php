2be7acc00a54d7db9fc42e548f666aa1e1e7a8fc
David Novicoff <davidnovicoff@gmail.com>
2019-07-14 12:31:35 -0500

    Admin routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -59,7 +59,7 @@ $route['about'] = 'door/index';
 $route['pass'] = 'door/pass';
 $route['create'] = 'door/create';
 
-$route['admin/cam/(select|update|delete)/(\d+)'] = 'admin/cam/$1/$2';
+$route['admin/cam/(select|insert|update|delete)/(\d?)'] = 'admin/cam/$1/$2';
 $route['admin/(loc|cam)'] = 'admin/$1';
 $route['admin'] = 'admin';
 
