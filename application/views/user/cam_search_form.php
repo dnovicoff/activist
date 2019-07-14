421c7bfba0a0bbd71f68065b6ff822d88dc733c1
David Novicoff <davidnovicoff@gmail.com>
2019-07-14 14:31:20 -0500

    User campaign search form

---
+++ application/views/user/cam_search_form.php
@@ -0,0 +1,104 @@
+
+
+<div class="container">
+	<?php echo form_open('user/search'); ?>
+		<table width="100%">
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo "Region: ";
+			?>
+			</td><td>
+			<?php
+				$data = array(
+					'name' => 'region',
+					'id' => '',
+					'value' => set_value('region'),
+					'class' => '',
+					'placeholder' => '<select>'
+				);
+				echo form_input($data);
+			?>
+			</td><td>
+			<?php
+				if (!empty(form_error('region')))  {
+					echo '<div style="font-size: 80%; color: red;">'.
+						form_error('region').
+					'</div>';  
+				}
+			?>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo "State: ";
+			?>
+			</td><td>
+			<?php
+				$data = array(
+					'name' => 'state',
+					'id' => '',
+					'value' => set_value('state'),
+					'class' => '',
+					'placeholder' => '<select>'
+				);
+				echo form_input($data);
+			?>
+			</td><td>
+			<?php
+				if (!empty(form_error('state')))  {
+					echo '<div style="font-size: 80%; color: red;">'.
+						form_error('state').
+					'</div>';  
+				}
+			?>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo "City ";
+			?>
+			</td><td>
+			<?php
+				$data = array(
+					'name' => 'city',
+					'id' => '',
+					'value' => set_value('cam_text'),
+					'class' => '',
+					'placeholder' => '<select>'
+				);
+				echo form_input($data);
+			?>
+			</td><td>
+			<?php
+				if (!empty(form_error('city')))  {
+					echo '<div style="font-size: 80%; color: red;">'.
+						form_error('city').
+					'</div>';  
+				}
+			?>
+			</td></tr>
+			<tr><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_reset("reset", "Reset", $attributes);
+			?>
+			</td><td>
+			<?php
+				$attributes = array(
+					'class' => ''
+				);
+				echo form_submit("submit", "Submit", $attributes);
+			?>
+			</td></tr>
+		</table>
+	<?php  echo form_close(); ?>
+</div>
