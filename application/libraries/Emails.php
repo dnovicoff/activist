43e9d6309e4dbb55c7a2a7d19fa93f16dc360938
David Novicoff <davidnovicoff@gmail.com>
2019-07-20 14:45:46 -0500

    Email functionality

--- application/libraries/Emails.php
+++ application/libraries/Emails.php
@@ -4,9 +4,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Emails {
 
 	private function configure()  {
-		$config['protocol'] = config_item('protocol');
+		$config['protocol']    = config_item('protocol');
+		$config['smtp_host']    = config_item('smtp_host');
+		$config['smtp_port']    = config_item('smtp_port');
+		$config['smtp_timeout'] = config_item('smtp_timeout');
+		$config['smtp_user']    = config_item('smtp_user');
+		$config['smtp_pass']    = config_item('smtppass');
+		$config['charset']    = config_item('charset');
+		$config['newline']    = config_item('newline');
+		$config['mailtype'] = config_item('mailtype'); // or html
+		$config['validate'] = config_item('validate'); // bool whether to validate email or not      
+
 		$config['mailpath'] = config_item('mailpath');
-		$config['charset'] = config_item('charset');
 		$config['wordwrap'] = config_item('wordwrap');
 
 		return $config;
@@ -14,8 +23,10 @@ class Emails {
 
 	public function send($to = FALSE, $msg = FALSE)  {
 		if ($to !== FALSE && $msg !== FALSE)  {
+			$this->CI->email->initialize($this->configure());
+
 			$this->CI->email->to($to);
-        		$this->CI->email->from('jo@activist');
+        		$this->CI->email->from('jo@activist.com');
         		$this->CI->email->subject('Password Reset');
         		$this->CI->email->message($msg);
         		$this->CI->email->send();
