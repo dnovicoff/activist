107a6e22dc49e261523998ff2d8c93e3878e2bff
David Novicoff <davidnovicoff@gmail.com>
2019-07-19 19:45:56 -0500

    Library for sending email

--- application/libraries/Emails.php
+++ application/libraries/Emails.php
@@ -3,10 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Emails {
 
+	public function send($to = FALSE, $msg = FALSE)  {
+		if ($to !== FALSE && $msg !== FALSE)  {
+			$this->email->to($to);
+        		$this->email->from('jo@activist');
+        		$this->email->subject('Password Reset');
+        		$this->email->message($msg);
+        		$this->email->send();
+		}
+
+		return FALSE;
+	}
+
 	public function __construct($params = array())
 	{
 		$this->CI =& get_instance();
 
 		$this->CI->load->helper('email');
+		$this->CI->load->config('emails');
 	}
 }
