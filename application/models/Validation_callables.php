fa226d269974477162b040692521efd8401a7529
David Novicoff <davidnovicoff@gmail.com>
2019-07-29 18:31:49 -0500

    For rules testing

--- application/models/Validation_callables.php
+++ application/models/Validation_callables.php
@@ -133,7 +133,7 @@ class Validation_callables extends MY_Model {
 				$error = 'Arizona ID SSN, or (A, B, D, or Y) + 8 numeric';
 				break;
 			case 'arkansas':
-				$regex = '^\d{9}$'; ## Test form input for zero entry
+				$regex = '^\d{9}$';
 				$error = 'Arkansas ID 8 numeric with 0 in front or 9 numeric';
 				break;
 			case 'california':
@@ -213,114 +213,129 @@ class Validation_callables extends MY_Model {
 				$error = 'Michigan ID 1 letter + 12 numeric';
 				break;
 			case 'minnesota':
-				$regex = '[a-zA-Z]{1}\d{12}';
+				$regex = '^[a-zA-Z]{1}\d{12}$';
 				$error = 'Minnesota 1 letter + 12 numeric';
 				break;
 			case 'mississippi':
-				$regex = '\d{9}';
+				$regex = '^\d{9}$';
 				$error = 'Mississippi ID 9 numeric';
 				break;
 			case 'missouri':
-				$regex = '[a-zA-Z0-9]{1}\d{5,9}';
+				$regex = '[a-zA-Z0-9]{1}\d{5,9}';  ## Look up exact spec on count
 				$error = 'Missouri ID 9 numeric or 1 letter + 5 to 9 numeric';
 				break;
 			case 'montana':
-				$regex = '';
+				if (strlen($state_id_no) === 9)  {
+					$regex = '^[a-zA-Z0-9]{9}$';
+				}  else  {
+					$regex = '^\d{13}$';
+				}
 				$error = 'Montana ID 13 numeric or 9 characters';
 				break;
 			case 'nebraska':
-				$regex = '[ABCEGHV]{1}\d{3,8}';
-				$error = 'Nebraska ID 1 letter + 3 to 8 numeric';
+				$regex = '[ABCEGHV]{1}\d{3,8}'; ## Check for numeric limit
+				$error = 'Nebraska ID 1 letter (A, B, C, E, G, H, or V)  + 3 to 8 numeric';
 				break;
 			case 'nevada':
-				$regex = '';
-				$error = 'Nevada ID';
+				if (strlen($state_id_no) === 10)  {
+					$regex = '^\d{10}$';
+				}  else if (strlen($state_id_no) === 12)  {
+					$regex = '^\d{12}$';
+				}  else  {
+					$regex = '^X\d{8}$';
+				}
+				$error = 'Nevada ID 10 numeric or 12 numeric or X + 8 numeric';
 				break;
 			case 'newhamshire':
-				$regex = '\d{2}[a-zA-Z]{3}\d{5}';
+				$regex = '^\d{2}[a-zA-Z]{3}\d{5}$';
 				$error = 'New Hamshire ID 2 numeric + 3 letters + 5 numeric';
 				break;
 			case 'newjersey':
-				$regex = '[a-zA-Z]{1}\d{14}';
+				$regex = '^[a-zA-Z]{1}\d{14}$';
 				$error = 'New Jersey 1 letter + 14 numeric';
 				break;
 			case 'newmexico':
-				$regex = '\d{9}';
+				$regex = '^\d{9}$';
 				$error = 'New Mexico ID 9 numeric';
 				break;
 			case 'newyork':
-				$regex = '[a-zA-Z]{1}\d{8,18}';
+				if (strlen($state_id_no) === 9)  {
+					$regex = '^\d{9}$';
+				}  else  {
+					$regex = '^[a-zA-Z]{1}\d{18}$';
+				}
 				$error = 'New York ID 9 numeric or 1 letter + 18 numeric';
 				break;
 			case 'northcarolina':
-				$regex = '\d{1,12}';
+				$regex = '^\d{1,12}$'; ## Get spec's on length
 				$error = 'North Carolina 1 to 12 numeric';
 				break;
 			case 'northdakota':
-				$regex = '[a-zA-Z0-9]{3}\d{6}';
+				$regex = '^[a-zA-Z0-9]{3}\d{6}$';
 				$error = 'North Dakota ID 9 numeric or 3 letters + 6 numeric';
+				break;
 			case 'ohio':
-				$regex = '[a-zA-Z]{2}\d{6}';
+				$regex = '^[a-zA-Z]{2}\d{6}$';
 				$error = 'Ohio ID 2 letters + 6 numeric';
 				break;
 			case 'oklahoma':
-				$regex = '[a-zA-Z]?\d{9}';
+				$regex = '^[a-zA-Z]?\d{9}$';
 				$error = 'Oklahoma ID 1 letter + 9 numeric or 9 numeric';
 				break;
 			case 'oregon':
-				$regex = '\d{1,9}';
+				$regex = '^\d{1,9}$';  ## Get spec's on numeric count
 				$error = 'Oregon ID 1 to 9 numeric';
 				break;
 			case 'pennsylvania':
-				$regex = '\d{8}';
+				$regex = '^\d{8}$';
 				$error = 'Pennyslvania ID 8 numeric';
 				break;
 			case 'rhodeisland':
-				$regex = '[V]?\d{6,7}';
+				$regex = '^[V0-9]{1}\d{6}$';
 				$error = 'Rhode Island ID 7 numeric or V + 6 numeric';
 				break;
 			case 'southcarolina':
-				$regex = '\d{1,10}';
+				$regex = '^\d{1,10}$';  ## Get spec's on numeric count
 				$error = 'South Carolina 1 to 18 numeric';
 				break;
 			case 'southdakota':
-				$regex = '\d{6,9}';
+				$regex = '^\d{6,9}$';  ## Get specifics on numeric count
 				$error = 'South Dakota ID 6 or 8 numeric or ssn';
 				break;
 			case 'tennessee':
-				$regex = '\d{7,9}';
+				$regex = '^\d{7,9}$';  ## Get specifics on numeric count
 				$error = 'Tennessee ID 7 to 9 numeric';
 				break;
 			case 'texas':
-				$regex = '\d{8}';
+				$regex = '^\d{8}$';
 				$error = 'Texas ID 8 numeric';
 				break;
 			case 'utah':
-				$regex = '\d{4,9}';
+				$regex = '^\d{4,9}$';  ## Get specifics on numeric count
 				$error = 'Utah ID 4 to 9 numeric';
 				break;
 			case 'vermont':
-				$regex = '\d{7}[a-zA-Z0-9]{1}';
+				$regex = '^\d{7}[a-zA-Z0-9]{1}$';
 				$error = 'Vermont ID 8 numeric or 7 numeric + 1 letter';
 				break;
 			case 'virginia':
-				$regex = '[a-zA-Z0-9]{1}\d{8}';
+				$regex = '^[a-zA-Z0-9]{1}\d{8}$';
 				$error = 'Virginia ID 9 numeric or 1 letter + 8 numeric';
 				break;
-			case 'washington':
-				$regex = '[a-zA-Z\*]{7}\d{3}[a-zA-Z0-9]{2}';
+			case 'washington': ## Double check rule for asterisk placement
+				$regex = '^[a-zA-Z\*]{7}\d{3}[a-zA-Z0-9]{2}$';
 				$error = 'Washington ID 7 letters or astericks ';
 				break;
 			case 'westvirginia':
-				$regex = '[a-zA-Z0-9]{7}';
+				$regex = '^[a-zA-Z0-9]{7}$';
 				$error = 'West Virginia 7 characters';
 				break;
 			case 'wisconsin':
-				$regex = '[a-zA-Z]{1}\d{13}';
+				$regex = '^[a-zA-Z]{1}\d{13}$';
 				$error = 'Wisconsin ID 1 letter + 13 numeric';
 				break;
 			case 'wyoming':
-				$regex = '\d{9}';
+				$regex = '^\d{9}$';
 				$error = 'Wyoming ID 9 numeric';
 				break;
 		}
