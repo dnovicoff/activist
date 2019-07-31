515d9681b1df6c56ee7a285570fb3f6336d34dcf
David Novicoff <davidnovicoff@gmail.com>
2019-07-31 10:07:15 -0500

    Mobile preparation

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,36 +1,35 @@
 
 <div class="container">
 	<div class="smallcol">
-		Search types<br />
+		<br /><br />
+		<a href="/user/">Active</a><br />
 		<a href="/user/search">Drill</a><br />
 		<a href="/user/national">National</a><br >
 		<a href="/user/current">Current</a><br />
 	</div>
 	<div class="largecol">
-		<?php
-			$output = '';
-			$page = $this->uri->segment(2);
-			if (!empty($page))  {
-				switch ($page)  {
-					case 'search':
-						$output = $this->load->view('user/cam_search_form', $data, TRUE);
-						break;
-					case 'show':
-						$output = $this->load->view('user/data', $data, TRUE);
-						break;
-					case 'detail':
-						$output = $this->load->view('user/data', $data, TRUE);
-						break;
-					case 'sign':
-						$output = $this->load->view('user/cam_sign_form', $data, TRUE);
-						break;
-				}
-			}  else  {
-				$output = 'Probable a graph of current campaigns being signed.';
-			}
+	<?php
+		$output = '';
+		$page = $this->uri->segment(2);
+		switch ($page)  {
+			case 'search':
+				$output = $this->load->view('user/cam_search_form', $data, TRUE);
+				break;
+			case 'show':
+				$output = $this->load->view('user/data', $data, TRUE);
+				break;
+			case 'detail':
+				$output = $this->load->view('user/data', $data, TRUE);
+				break;
+			case 'sign':
+				$output = $this->load->view('user/cam_sign_form', $data, TRUE);
+				break;
+			default:
+				$output = $this->load->view('user/cam_graph', $data, TRUE);
+		}
 
-			echo $output;
-		?>
+		echo $output;
+	?>
 	</div>
 	<div class="smallcol">
 		User identifier information if possible
