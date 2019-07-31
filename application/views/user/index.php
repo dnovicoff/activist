cda91eecfd84709d93f0ad00661e7ba38746631b
David Novicoff <davidnovicoff@gmail.com>
2019-07-31 18:24:19 -0500

    New Layout

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -2,10 +2,9 @@
 <div class="container">
 	<div class="smallcol">
 		<br /><br />
-		<a href="/user/">Active</a><br />
-		<a href="/user/search">Drill</a><br />
-		<a href="/user/national">National</a><br >
-		<a href="/user/current">Current</a><br />
+	<?php
+		$this->load->view('gen/collapsible', $data);
+	?>
 	</div>
 	<div class="largecol">
 	<?php
@@ -13,19 +12,19 @@
 		$page = $this->uri->segment(2);
 		switch ($page)  {
 			case 'search':
-				$output = $this->load->view('user/cam_search_form', $data, TRUE);
+				$output .= $this->load->view('user/cam_search_form', $data, TRUE);
 				break;
 			case 'show':
-				$output = $this->load->view('user/data', $data, TRUE);
+				$output .= $this->load->view('user/data', $data, TRUE);
 				break;
 			case 'detail':
-				$output = $this->load->view('user/data', $data, TRUE);
+				$output .= $this->load->view('user/data', $data, TRUE);
 				break;
 			case 'sign':
-				$output = $this->load->view('user/cam_sign_form', $data, TRUE);
+				$output .= $this->load->view('user/cam_sign_form', $data, TRUE);
 				break;
 			default:
-				$output = $this->load->view('user/cam_graph', $data, TRUE);
+				$output .= $this->load->view('user/cam_graph', $data, TRUE);
 		}
 
 		echo $output;
