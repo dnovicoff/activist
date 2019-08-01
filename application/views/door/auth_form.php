5039ba13cb87f64c8bf90fe9d011a82aa27b392c
David Novicoff <davidnovicoff@gmail.com>
2019-07-31 21:55:20 -0500

    Code reuse

--- application/views/door/auth_form.php
+++ application/views/door/auth_form.php
@@ -15,74 +15,72 @@
 		);
 	?>
 	<?php echo form_open($login_url, $attributes); ?>
-		<table width="100%">
-			<tr>
-			<?php
-				$attributes = array(
-					'class' => ''
-				);
-				echo "Email";
-			?>
-			</tr>
-			<tr>
-			<?php
-				$data = array(
-					'name' => 'login_string',
-					'id' => 'login_string',
-					'value' => htmlspecialchars_decode(set_value('login_string'), ENT_QUOTES),
-					'class' => '',
-					'placeholder' => ''
-				);
-				echo form_input($data);
-			?>
-			</tr>
-			<tr>
-				<?php
-					if (!empty(form_error('login_string')))  {
-						echo '<div style="font-size: 80%; color: red;">'.
-							form_error('login_string').
-						'</div>';
-					}
-				?>
-			</tr>
-			<tr>
-			<?php
-				$attributes = array(
-					'class' => ''
-				);
-				echo "Password";
-			?>
-			</tr>
-			<tr>
-			<?php
-				$data = array(
-					'type' => 'password',
-					'name' => 'login_pass',
-					'id' => 'login_pass',
-					'value' => '',
-					'class' => '',
-					'placeholder' => ''
-				);
-				echo form_input($data);
-			?>
-			</tr>
-			<tr>
-				<?php
-					if (!empty(form_error('login_pass')))  {
-						echo '<div style="font-size: 80%; color: red;">'.
-							form_error('login_pass').
-						'</div>';
-					}
-				?>
-			</tr>
-			<tr>
-			<?php
-				$attributes = array(
-					'class' => 'btn'
-				);
-				echo form_submit("submit", "Submit", $attributes);
-			?>
-			</tr>
-		</table>
+		<div class="row">
+		<?php
+			$attributes = array(
+				'class' => ''
+			);
+			echo "Email";
+		?>
+		</div>
+		<div class="row">
+		<?php
+			$data = array(
+				'name' => 'login_string',
+				'id' => 'login_string',
+				'value' => htmlspecialchars_decode(set_value('login_string'), ENT_QUOTES),
+				'class' => '',
+				'placeholder' => ''
+			);
+			echo form_input($data);
+		?>
+		</div>
+		<div class="row">
+		<?php
+			if (!empty(form_error('login_string')))  {
+				echo '<div style="font-size: 80%; color: red;">'.
+					form_error('login_string').
+				'</div>';
+			}
+		?>
+		</div>
+		<div class="row">
+		<?php
+			$attributes = array(
+				'class' => ''
+			);
+			echo "Password";
+		?>
+		</div>
+		<div class="row">
+		<?php
+			$data = array(
+				'type' => 'password',
+				'name' => 'login_pass',
+				'id' => 'login_pass',
+				'value' => '',
+				'class' => '',
+				'placeholder' => ''
+			);
+			echo form_input($data);
+		?>
+		</div>
+		<div class="row">
+		<?php
+			if (!empty(form_error('login_pass')))  {
+				echo '<div style="font-size: 80%; color: red;">'.
+					form_error('login_pass').
+				'</div>';
+			}
+		?>
+		</div>
+		<div class="row">
+		<?php
+			$attributes = array(
+				'class' => 'btn'
+			);
+			echo form_submit("submit", "Submit", $attributes);
+		?>
+		</div>
 	<?php  echo form_close(); ?>
 </div>
