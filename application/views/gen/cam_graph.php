aa2244bffcd5aea98391c97c80c8fc8b9e78c8f2
David Novicoff <davidnovicoff@gmail.com>
2019-08-03 12:29:51 -0500

    Buggy

--- application/views/gen/cam_graph.php
+++ application/views/gen/cam_graph.php
@@ -4,5 +4,11 @@
 	<div class="graph_canvas">
 		<img src="/img/unitedstates_map.jpg" height="327" width="488">	
 	</div>
-	<a href="/admin/cam/select/<?php if (isset($cam_id)) {  echo $cam_id;  } else {  echo '';  } ?>">View</a>
+	<?php
+		if (isset($cam_id))  {
+	?>
+		<a href="/admin/cam/select/<?php if (isset($cam_id)) {  echo $cam_id;  } else {  echo '';  } ?>">View</a>
+	<?php
+		}
+	?>
 </div>
