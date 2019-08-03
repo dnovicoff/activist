073252dcfe758f34617a95f0f5a5fe6575c0f84e
David Novicoff <davidnovicoff@gmail.com>
2019-08-03 11:44:13 -0500

    Will be called from two controllers

--- application/views/gen/cam_graph.php
+++ application/views/gen/cam_graph.php
@@ -4,5 +4,5 @@
 	<div class="graph_canvas">
 		<img src="/img/unitedstates_map.jpg" height="327" width="488">	
 	</div>
-	<a href="/admin/cam/select/<?php echo $cam_id  ?>">View</a>
+	<a href="/admin/cam/select/<?php if (isset($cam_id)) {  echo $cam_id;  } else {  echo '';  } ?>">View</a>
 </div>
