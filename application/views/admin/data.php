6c8bf0f72fa9cc6b622178b49f081cdfc123f5d9
David Novicoff <davidnovicoff@gmail.com>
2019-08-04 07:50:43 -0500

    Data page accessed by two controllers

--- application/views/admin/data.php
+++ application/views/admin/data.php
@@ -1,8 +1,7 @@
 
-
 <div class="container">
-	Campaigns total: 0<br />
-	Current campaigns: 0<br />
-	Average signing: 0 signatures per hour<br />
-	Top campaign: "The night the lights went out in georga"<br />	
+	Campaigns total: <?php echo $cam_total[0]['total']; ?><br />
+	Current campaigns: <?php echo $cam_current[0]['total']; ?><br />
+	Average signatures per hour: <?php echo $sign_avg; ?><br />
+	Top campaign: "<?php echo $cam_top; ?>"<br />	
 </div>
